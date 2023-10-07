56f4bbc40b07956412144b76c4e12c60f13c2ef2
hammad-air <hammadn788@gmail.com>
2023-10-07 22:50:42 +0500

    repo added

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/users', (req, res) => {
+    res.send('Hello World!')
+  })
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 })
